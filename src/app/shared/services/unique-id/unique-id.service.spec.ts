84f811a33c75309139809a75931b96f06cb6f753
Matheus eli <matheuselisouzaferreira@hotmail.com>
2022-01-17 23:18:35 -0300

    first commit

---
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -0,0 +1,9 @@
+describe('artefato que queremos testar', () => {
+    it('primeira condicao que queremos testar', () => {
+
+    });
+
+    it('segunda condicao que queremos testar', () => {
+
+    });
+});
---
+++ src/app/shared/services/unique-id/unique-id.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class UniqueIdService {
+    private numberOfGeneratedIds = 0;
+
+    public generateUniqueIdWithPrefix(prefix: string): string {
+        if (!prefix) {
+            throw Error('Prefix cannot be empty!');
+        }
+        const uniqueId = this.generateUniqueId();
+        this.numberOfGeneratedIds++;
+        return `${prefix}-${uniqueId}`;
+    }
+
+    public getNumberOfGeneratedUniqueIds(): number {
+        return this.numberOfGeneratedIds;
+    }
+
+    private generateUniqueId(): string {
+        return uuidv4();
+    }
+}
