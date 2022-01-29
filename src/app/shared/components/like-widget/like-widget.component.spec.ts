bfc10be753bdb3aec116f3b9847d72ba25c23b03
MatheusEli <matheuselisouzaferreira@hotmail.com>
2022-01-28 22:04:10 -0300

    utilizando spy

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -34,10 +34,17 @@ describe(LikeWidgetComponent.name,() => {
 
   it(`#${LikeWidgetComponent.prototype.like.name}
   should trigger emission when called`, () => {
+
+    spyOn(component.liked, 'emit');
     fixture.detectChanges();
-    component.liked.subscribe(() => {
-      expect(true).toBeTrue();
-    })
     component.like();
-  })
+    expect(component.liked.emit).toHaveBeenCalled();
+
+    /*component.liked.subscribe(() => {
+      expect(true).toBeTrue();
+      done();
+    });
+    component.like();*/
+
+  });
 });
